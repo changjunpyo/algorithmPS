62627f14c6abab8ec7d45054e6bb8c1783fe3cc0
changjunpyo <changjunpyo@gmail.com>
2020-02-13 17:02:27 +0900

    BOJ segtree with lazy propa

--- baekjoon/10999.cpp
+++ baekjoon/10999.cpp
@@ -12,30 +12,44 @@ long long init (vector<long long> &a, vector<long long> &tree, int node, int sta
     return tree[node];
 }
 
-long long sum (vector<long long> &tree,int node ,int start, int end, int left , int right ){
+long long lazy_propa_sum(vector<long long> &tree, vector<long long> &change,int node ,int start, int end, int left , int right ){
+    if (change[node] != 0){
+        tree[node] += change[node]*(end-start+1);
+        if (start != end){
+            change[node*2] += change[node];
+            change[node*2+1] += change[node];
+        }
+        change[node] = 0;
+    }
     if (end < left || start > right)
         return 0;
-    if (left <= start && end <= right)
+
+    if (left <= start && end <= right){
         return tree[node];
-    else
-        return sum(tree,node*2,start,(start+end)/2,left,right)+sum(tree,node*2+1,(start+end)/2+1,end,left,right);
+    }
+    else{
+        change[node*2] += change[node];
+        change[node*2+1] += change[node];
+        change[node] = 0;
+        return lazy_propa_sum(tree,change,node*2,start,(start+end)/2,left,right)+lazy_propa_sum(tree,change,node*2+1,(start+end)/2+1,end,left,right);
+    }
 }
 
-void update(vector<long long> &tree, int node, int start, int end, int left,int right, long long diff){
+void update(vector<long long> &tree, vector<long long> &change, int node, int start, int end, int left,int right, long long diff){
     if (start > right || end < left)
         return ;
-    if (start == end)
-        tree[node] += diff;
-    else{
-        if (left <= start && end <= right)
-            tree[node] += diff*(end-start+1);
-        else if (left <= start)
-            tree[node] += diff*(right -start+1);
-        else if (end <= right)
-            tree[node] += diff*(end - left +1);
-        update(tree,node*2,start,(start+end)/2,left,right,diff);
-        update(tree,node*2+1,(start+end)/2+1,end,left,right,diff);
+    if (left <= start && end <= right){
+        change[node] += diff;
+        return;
     }
+    if (start <= left && right <= end)
+        tree[node] += (right-left+1)*diff;
+    else if (start <= left && left <= end)
+        tree[node] += (end - left+1)*diff;
+    else if (start <= right && right <=end)
+        tree[node] += (right - start+1)*diff;
+    update(tree,change,node*2,start,(start+end)/2,left,right,diff);
+    update(tree,change,node*2+1,(start+end)/2+1,end,left,right,diff);
     return;
 }
 int main(){
@@ -47,7 +61,7 @@ int main(){
     int h = (int) ceil(log2(N));
     int tree_size = (1<<(h+1));
     vector <long long> a(N);
-    vector <bool> change(N);
+    vector <long long> change(tree_size,0);
     vector <long long> tree(tree_size);
     for (int i=0; i<N; i++)
         cin >>a[i];
@@ -58,14 +72,14 @@ int main(){
         if (x==2){
            int y,z;
            cin >>y >>z;
-           cout <<sum(tree,1,0,N-1,y-1,z-1) <<"\n";
+           cout <<lazy_propa_sum(tree,change,1,0,N-1,y-1,z-1) <<"\n";
         }
         else{
             int y;
             int _y;
             long long z;
             cin >> y >> _y >> z;
-            update(tree, 1, 0,N-1,y-1, _y-1,z);
+            update(tree,change, 1, 0,N-1,y-1, _y-1,z);
             a[y-1] = z;
         }
     }
