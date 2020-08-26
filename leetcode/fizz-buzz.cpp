52e707fccaa51aecdc349d11103b651634201f95
changjunpyo <changjunpyo@gmail.com>
2020-08-26 17:38:33 +0900

    LC 08/26

--- leetcode/fizz-buzz.cpp
+++ leetcode/fizz-buzz.cpp
@@ -3,15 +3,17 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans(n);
         for (int i=1; i<=n; i++){
-            string s=to_string(i);
-            if (i%15== 0)
-                s = "FizzBuzz";
-            else if (i%3 == 0)
-                s = "Fizz";
-            else if (i%5 == 0)
-                s = "Buzz";
-            
-            ans[i-1] = s;
+            string s ="";
+            if (i%3 ==0){
+                s.append("Fizz");    
+            }
+            if (i%5 ==0){
+                s.append("Buzz"); 
+            } 
+            if (i%3 != 0 && i%5 != 0){
+                s.append(to_string(i));
+            }
+            ans[i-1]=s;
         }
         return ans;
     }
